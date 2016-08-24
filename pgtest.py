3c8b45043dc12d058de3acdead3e86df933fb8c8
ChrisShiu <Leaptosora@gmail.com>
2016-08-24 15:48:12 +0800

    full

--- pgtest.py
+++ pgtest.py
@@ -1,35 +1,43 @@
+# import the pygame module
 import pygame
+
+# import pygame.locals for easier access to key coordinates
 from pygame.locals import *
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+# initialize pygame
 pygame.init()
-screen = pygame.display.set_mode((800,600))
+
+# create the screen object
+# here we pass it a size of 800x600
+screen = pygame.display.set_mode((800, 600))
+
+player = Player()
 
 # Variable to keep our main loop running
 running = True
 
 # Our main loop!
 while running:
-   # for loop through the event queue
-   for event in pygame.event.get():
-       # Check for KEYDOWN event; KEYDOWN is a constant defined in pygame.locals, which we imported earlier
-       
-
-       # Create the surface and pass in a tuple with its length and width
-       surf = pygame.Surface((50, 50))
-
-       # Give the surface a color to differentiate it from the background
-       surf.fill((255, 255, 255))
-       rect = surf.get_rect()
-
-       # This line says "Draw surf onto screen at coordinates x:400, y:300"
-       screen.blit(surf, (400, 300))
-       pygame.display.flip()
-
-       if event.type == KEYDOWN:
-           # If the Esc key has been pressed set running to false to exit the main loop
-           if event.key == K_ESCAPE:
-               running = False
-       # Check for QUIT event; if QUIT, set running to false
-       elif event.type == QUIT:
-           running = False
-
+# for loop through the event queue
+    for event in pygame.event.get():
+# Check for KEYDOWN event; KEYDOWN is a constant defined in pygame.locals, which we imported earlier
+        if event.type == KEYDOWN:
+# If the Esc key has been pressed set running to false to exit the main loop
+            if event.key == K_ESCAPE:
+                running = False
+# Check for QUIT event; if QUIT, set running to false
+        elif event.type == QUIT:
+                running = False
+    
+    # Draw the player to the screen
+    screen.blit(player.surf, (400, 300))
+    
+    # Update the display
+    pygame.display.flip()
