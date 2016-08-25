dc252ab9e61dffd31f6add44c11663966fd33f01
ChrisShiu <Leaptosora@gmail.com>
2016-08-25 11:09:18 +0800

    增加会飞的紫嫣

--- helloworld.py
+++ helloworld.py
@@ -2,26 +2,31 @@
 
 import pygame
 from pygame.locals import *
-from sys import exit
 
-background_image = 'sushiplate.jpg'
-mouse_image = 'fugu.png'
+background_image = r'D:\test\learnPyGame\world.png'
+mouse_image = r'D:\test\learnPyGame\2.png'
+running = True
 
 pygame.init()
 
 screen = pygame.display.set_mode((640,480), 0, 32)
-pygame.display.set_caption("这是什么Fucking游戏")
+pygame.display.set_caption("红红火火恍恍惚惚")
 
-backround = pygame.image.load(background_image).convert()
-mouse_cursor = pygame.image.load(mouse_image).convert.alpha()
+background = pygame.image.load(background_image).convert()
+mouse_cursor = pygame.image.load(mouse_image).convert_alpha()
 
-while True:
-	for event in pygame.ecent.get():
-		if event.type == QUIT:
-			exit()
-	screen.blit(backround, (0,0))
+while running:
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+        elif event.type == QUIT:
+            running = False
+
+	screen.blit(background, (0,0))
 	x, y = pygame.mouse.get_pos()
+	pygame.mouse.set_visible(False)
 	x-= mouse_cursor.get_width() / 2
-	y-= mouse_cursor.gei_height() / 2
+	y-= mouse_cursor.get_height() / 2
 	screen.blit(mouse_cursor, (x,y))
 	pygame.display.update()
